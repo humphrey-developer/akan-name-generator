69e4c4c16f7c3dcd053165d20bb5be47ef0007fb
humphrey-developer <juniorcodyt50@gmail.com>
2026-08-01 20:52:39 +0300

    feat:finished setting up my listener

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// SETTING UP EVENT LISTENER
-function handleFormSubmit(event) {
-    event.preventDefault();
-    console.log("Form submission got")
-}
-// selecting form from my HTML
-const formElement = document.getElementById("form_section");
-formElement.addEventListener("submit",handleFormSubmit);
-
-// READING USER INPUT FROM HTML
-
-function handleFormSubmit(event) {
-    event.preventDefault();
-
-    const birthdateValue = document.getElementById("birthdate").value;
-    const selectedGender = document.querySelector('input[name="gender"]:checked').value;
-
-    console.log("Date chosen:", birthdateValue);
-    console.log("Gender chosen:", selectedGender);
-}
-
-const formElement = document.getElementById("form");
-formElement.addEventListener("submit", handleFormSubmit);
-
-
-
-
 // AKAN NAMES EDITS 
 // DAY OF WEEK
 
@@ -37,6 +10,39 @@ const male_names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 // FEMALE NAMES
 
 const female_names = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+
+
+// SETTING UP EVENT LISTENER
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+     const birthdateValue = document.getElementById("birthdate").value;
+    const selectedGender = document.querySelector('input[name="gender"]:checked').value;
+
+    const dateParts = birthdateValue.split("-"); 
+
+    const year = parseInt(dateParts[0], 10); 
+    const MM   = parseInt(dateParts[1], 10); 
+    const DD   = parseInt(dateParts[2], 10); 
+
+    const CC = Math.floor(year / 100);     
+    const YY = year % 100;
+
+    console.log("Gender:", selectedGender);
+    console.log(`CC: ${CC}, YY: ${YY}, MM: ${MM}, DD: ${DD}`);
+}    
+
+   
+// selecting form from my HTML
+const formElement = document.getElementById("form_section");
+formElement.addEventListener("submit",handleFormSubmit);
+
+
+
+
+
+
 
 
 
