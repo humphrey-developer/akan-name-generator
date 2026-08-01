9483120a2dac14fdfa80f71fe29068e6445819bf
humphrey-developer <juniorcodyt50@gmail.com>
2026-08-01 19:28:19 +0300

    feat:created a variable for male names

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // AKAN NAMES EDITS 
 // DAY OF WEEK
 
-const days_Of_Week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]  
+const days_of_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]  
+
+// MALE NAMES
+
+const male_names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+
 
 
 
