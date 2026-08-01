611459b420e93c523495c1667224dc661f3d088b
humphrey-developer <juniorcodyt50@gmail.com>
2026-08-01 20:20:05 +0300

    feat:created a comment fot the reading user input from HTML and with the function i had created i used the necessary .js methods to call out

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ function handleFormSubmit(event) {
 const formElement = document.getElementById("form_section");
 formElement.addEventListener("submit",handleFormSubmit);
 
+// READING USER INPUT FROM HTML
 
+function handleFormSubmit(event) {
+    event.preventDefault();
 
+    const birthdateValue = document.getElementById("birthdate").value;
+    const selectedGender = document.querySelector('input[name="gender"]:checked').value;
+
+    console.log("Date chosen:", birthdateValue);
+    console.log("Gender chosen:", selectedGender);
+}
+
+const formElement = document.getElementById("form");
+formElement.addEventListener("submit", handleFormSubmit);
 
 
 
