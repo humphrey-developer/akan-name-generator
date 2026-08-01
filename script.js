2aa90837cc343288c0b2fff60a1859fbb729a4e5
humphrey-developer <juniorcodyt50@gmail.com>
2026-08-01 20:00:01 +0300

    feat:added the listener in my .js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+// SETTING UP EVENT LISTENER
+const formSection = document.getElementById("form_section");
+formSection.addEventListener("submit",function(event) {
+    event.preventDefault();
+    console.log("")
+})
+
+
+
+
+
 // AKAN NAMES EDITS 
 // DAY OF WEEK
 
-const days_of_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]  
+const days_of_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 // MALE NAMES
 
-const male_names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+const male_names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
 // FEMALE NAMES
 
-const female_names = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+const female_names = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
 
 
 
