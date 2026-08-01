e8f859cf05789009757120007a0409764da7ee8f
humphrey-developer <juniorcodyt50@gmail.com>
2026-08-01 19:31:11 +0300

    feat:created a variable for female names

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const days_of_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday
 
 const male_names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 
+// FEMALE NAMES
+
+const female_names = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+
 
 
 
